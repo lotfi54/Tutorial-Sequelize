0067994317eb4dc69071d4d288905afbade573ac
lotfi54 <lotfibouraine@gmail.com>
2021-12-16 14:19:24 +0100

    Connecion port 5050 plus installation des différent package

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-** BACKEND **
+**BACKEND**
 
 - [x] installer express  et body-parser 
 - [x] installer sequelize avec cette commande - npm install --save sequelize 
+- [x] Pour ce qui usilise mysql il faut également l'intaller - npm install --save msql2
 
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-Hello
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+
+app.get('/', (req,res) => {
+    res.send('index')
+})
+
+
+
+const PORT = process.env.PORT || 5050;
+
+
+app.listen(PORT, () =>
+  console.log("Started on port " + `Server is running ${PORT}`)
+);
